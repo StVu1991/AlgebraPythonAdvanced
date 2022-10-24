0ce82ba26870f16f595074d1b8d550dd8445007f
office10 <office10@ucione.local>
2022-10-24 10:15:37 +0200

    Initial commit

---
+++ car.py
@@ -0,0 +1,17 @@
+class Car:
+    axles = 2
+    wheels = 4
+    pass
+
+car1 = Car()
+car2 = Car()
+
+print(car1.wheels)
+print(car1.axles)
+print(car2.wheels)
+print(car2.axles)
+
+car2.wheels = 6
+
+print(car2.wheels)
+print(car2.axles)
---
+++ car_instance.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, manufacturer, model, price):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.price = price
+
+hgss_v1 = Car("Nissan", "Pajero", 1.00)
+print(hgss_v1.manufacturer)
+hgss_v2 = Car("Land Rover", "Discovery", 1.00)
+print(hgss_v2.manufacturer)
